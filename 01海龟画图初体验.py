6ff5f367191a02c454cb3444deb6a3fe0e3afe51
ht7813 <114791222+ht7813@users.noreply.github.com>
2023-09-30 16:49:14 +0800

    update

---
+++ 01海龟画图初体验.py
@@ -0,0 +1,20 @@
+import turtle
+
+t = turtle.Turtle()
+t.width(10)
+#t.penup()
+#t.goto(-100,100)
+#t.pendown()
+#t.goto(-100,-100)
+t.color('red')
+t.forward(150)
+t.right(90)
+t.forward(150)
+t.right(90)
+t.forward(150)
+t.right(90)
+t.forward(150)
+
+t.backward(200)
+
+turtle.done()
